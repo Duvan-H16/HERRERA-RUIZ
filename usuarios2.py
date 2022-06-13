1e5dc336e7423551b7385ae0b19e3e85415153f5
Duvan-H16 <rdherrera11@misena.edu.co>
2022-06-13 00:27:06 -0500

    commit-python12

--- usuarios2.py
+++ usuarios2.py
@@ -1,7 +1,6 @@
 import csv #Importar Modulo
-
-with open(csv.Usuarios) as archivo:
+nombre_archivo = "Usuarios.csv"
+with open(nombre_archivo, mode='r') as archivo:
     users = csv.reader(archivo)
     for dato in users:
-        clave = list(dato.keys())
-        print(clave[1], dato["Nombres"], clave[2], dato["Apellidos"], clave[4], dato["Dependencia"], clave[10], dato["Estado"], "N° Columnas =", len(dato))
+        print(dato)
