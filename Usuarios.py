ad76ec1982b0bc0d3ff0c717157e957ce57e2397
Duvan-H16 <rdherrera11@misena.edu.co>
2022-06-12 20:16:10 -0500

    commit-python7

---
+++ Usuarios.py
@@ -0,0 +1,11 @@
+import csv #Importar Modulo
+import os
+nombre_archivo = "usuarios.csv"
+ruta= os.path.abspath(__file__)
+directorio= os.listdir(os.path.dirname(ruta))
+print('los directorios son: ', directorio, "y se encuentran en la ruta:", os.path.dirname(ruta), '\n')
+with open(nombre_archivo, "r") as archivo:
+    users = csv.DictReader(archivo)
+    for dato in users:
+        clave = list(dato.keys())
+        print(clave[1], dato['Nombres'], clave[2], dato['Apellidos'], clave[4], dato['Dependencia'], clave[10], dato["Estado"], "N° Columnas =", len(dato))
