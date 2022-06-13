7bb8eae396f4a82584a8003357478aca384947b0
Duvan-H16 <rdherrera11@misena.edu.co>
2022-06-13 01:08:50 -0500

    commit-python13

--- Usuarios.py
+++ Usuarios.py
@@ -5,8 +5,7 @@ ruta= os.path.abspath(__file__)
 directorio= os.listdir(os.path.dirname(ruta))
 print('los directorios son: ', directorio, "y se encuentran en la ruta:", os.path.dirname(ruta), '\n')
 with open(nombre_archivo, "r") as archivo:
-    users = csv.DictReader(archivo)
+    users = csv.reader(archivo, delimiter='\n')
     for dato in users:
-        clave = list(dato.keys())
-        print(clave[1], dato["Nombres"], clave[2], dato["Apellidos"], clave[4], dato["Dependencia"], clave[10], dato["Estado"], "N° Columnas =", len(dato))
+        print("N° Columnas =", len(dato))
         
--- usuarios2.py
+++ usuarios2.py
@@ -1,6 +1,10 @@
 import csv #Importar Modulo
+import os
 nombre_archivo = "Usuarios.csv"
-with open(nombre_archivo, mode='r') as archivo:
-    users = csv.reader(archivo)
+ruta= os.path.abspath(__file__)
+directorio= os.listdir(os.path.dirname(ruta))
+print('los directorios son: ', directorio, "y se encuentran en la ruta:", os.path.dirname(ruta), '\n')
+with open(nombre_archivo) as archivo:
+    users = csv.reader(archivo, delimiter="\n")
     for dato in users:
         print(dato)
